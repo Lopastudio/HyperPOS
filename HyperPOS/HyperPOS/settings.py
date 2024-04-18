80cf66f3df1dd39cf621f8df11cdb8467ddea7d7
Patrik <ockonko@gmail.com>
2024-04-18 18:52:16 +0200

    sssssssssss

--- HyperPOS/HyperPOS/settings.py
+++ HyperPOS/HyperPOS/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&0n%c@6-o^i!r&n^^chrk8z5+*w&v^4bbbw&7$^6@y*!5ovf07'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
